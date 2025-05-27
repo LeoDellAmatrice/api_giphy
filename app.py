0b8b239314a4bc07fa55e50e6d1078e9621e96ab
LeoDellAmatrice <leonardo.dellamatrice@gmail.com>
2025-05-27 09:24:32 -0300

    v1.0

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask
+import os
 
+from flask import Flask, request, render_template
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
 app = Flask(__name__)
-
-
+GIPHY_API_KEY = os.getenv("API_KEY")
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
+
+@app.route('/search')
+def search():
+    query = request.args.get('query', '')
+    gifs = []
+
+    if query:
+        response = requests.get(
+            f'https://api.giphy.com/v1/gifs/search?api_key={GIPHY_API_KEY}&q={query}&limit=20'
+        )
+        if response.status_code == 200:
+            data = response.json()
+            gifs = [{
+                'url': gif['images']['original']['url'],
+                'title': gif['title']
+            } for gif in data['data']]
+
+    return render_template('index.html', gifs=gifs, query=query)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
